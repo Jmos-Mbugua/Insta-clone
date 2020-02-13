d83390a81e36feb04d8351a7f86df509d03bf7bc
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-02-14 02:46:49 +0300

    Configure static files and their urls in the project level folder

--- instaclone_project/urls.py
+++ instaclone_project/urls.py
@@ -13,10 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings 
+from django.conf.urls.static import static 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #django admin
     path('admin/', admin.site.urls),
+
+    #user managemenrt
+    path('accounts/', include('allauth.urls')),
+
+    #Local
+    path('', include('instagram.urls')),
     path('', include('pages.urls')),
-]
+    
+] 
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
