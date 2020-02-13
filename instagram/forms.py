847143d9d7a08532fdee056460a5cb3d0f0552bd
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-02-14 02:49:46 +0300

    Create a forms file which contains posting and search forms

---
+++ instagram/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post, Profile
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['image', 'caption', 'location_posted']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'avatar')
